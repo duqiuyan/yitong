59c6ea3b84bf2dcc97fc6a7bfe5f91622aa59c4d
duqiuyan <447442250@qq.com>
2016-08-01 17:35:16 +0800

    提交代码

---
+++ statics/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(".z01").click(function(){
+		$(".s01").toggle();
+	});
+	$(".z02").click(function(){
+		$(".s02").toggle();
+	});
+	$(".z03").click(function(){
+		$(".s03").toggle();
+	});
+	$(".z04").click(function(){
+		$(".s04").toggle();
+	});
+	$(".z05").click(function(){
+		$(".s05").toggle();
+	});
+});
